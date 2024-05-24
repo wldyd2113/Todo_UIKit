27e59e04ecf40250e3ffac4726867c6c4d1ca401
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-24 11:27:16 +0900

    table설정

--- TodoList/TodoList/View/MainViewController.swift
+++ TodoList/TodoList/View/MainViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDataSource{
+    let todos = [Todo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,9 +25,23 @@ class MainViewController: UIViewController {
         let addButon = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(navigateToAddPage))
         self.navigationItem.rightBarButtonItem = addButon
         
+        let tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        view.addSubview(tableView)
     }
     
-    //Mark: - AddViewController로 이동
+    //MARK: -UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let todoList = todos[indexPath.row]
+        return cell
+    }
+    
+    // AddViewController로 이동
     @objc func navigateToAddPage() {
         let addView = AddViewController()
         self.navigationController?.pushViewController(addView, animated: true)
