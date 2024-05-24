cb550a8ecd1ea787d9065bbc0d68b5aa85d1b87f
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-24 09:22:19 +0900

    MVC

---
+++ TodoList/TodoList/View/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  TodoList
+//
+//  Created by 차지용 on 5/24/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
