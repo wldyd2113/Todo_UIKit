b9cbe998d2b8da023ea175170b2c2e66b43e5af0
Cha, Jiyong <wldyd2113@gmail.com>
2024-05-24 09:59:03 +0900

    Navigation title 설정

--- TodoList/TodoList/View/MainViewController.swift
+++ TodoList/TodoList/View/MainViewController.swift
@@ -11,7 +11,21 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.backgroundColor = .white
+        
+        self.navigationItem.title = "TodoList"
+        let appearence = UINavigationBarAppearance()
+        appearence.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white] //navigation 제목 글자 색 지정
+        appearence.backgroundColor = .black
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearence
+        
+        let addButon = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(navigateToAddPage))
+        
+    }
+    
+    @objc func navigateToAddPage() {
+        
     }
 
 }
